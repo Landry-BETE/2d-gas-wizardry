64ccc5fe8ce9ee7e7daabd82e027b9a42d3d5fd8
jilb <landrybete@gmail.com>
2017-05-05 02:32:47 +0100

    Introduced numpy to shorten code and improve performance

--- game.py
+++ game.py
@@ -1,14 +1,13 @@
 from itertools import *
 
-import numpy as np
 import pygame
 
 from particle import *
 
-STARTING_BLUE_PARTICLES = 50
+STARTING_BLUE_PARTICLES = 100
 
-WIDTH = 400
-HEIGHT = 400
+WIDTH = 700
+HEIGHT = 700
 WHITE = (255, 255, 255)
 BLUE = (0, 0, 255)
 RED = (255, 0, 0)
@@ -19,68 +18,41 @@ pygame.display.set_caption("Monoatomic Bidimensional Gas")
 clock = pygame.time.Clock()
 
 
-def colliding(particle, other_particle):
+def colliding(particle, particle2):
     """checks if particles are overlaping & moving closer to one another to trigger bouncing & avoid agglomeration"""
-    overlap = np.linalg.norm(
-        np.array([particle.x, particle.y]) - np.array(
-            [other_particle.x, other_particle.y])) - particle.size - other_particle.size
-    convergence = np.inner(np.array([particle.move_x - other_particle.move_x, particle.move_y - other_particle.move_y]),
-                           np.array([particle.x - other_particle.x, particle.y - other_particle.y]))
-    return overlap < 0 and convergence < 0
+    return np.linalg.norm(particle.pos - particle2.pos) < particle.size + particle2.size and np.inner(
+        particle.speed - particle2.speed, particle.pos - particle2.pos) < 0
 
 
 def energy(particles):
     """total kinetic energy up to a constant multiplier"""
-    return np.sum(
-        np.array([np.linalg.norm(particle.size ** 2 * np.array([particle.move_x, particle.move_y])) ** 2 for particle in
-                  particles]))
+    return np.sum([particle.size ** 2 * np.linalg.norm(particle.speed) ** 2 for particle in particles])
 
 
 def momentum(particles):
     """total momentum up to a constant multiplier"""
-    return np.sum([np.array([particle.move_x, particle.move_y]) * particle.size ** 2 for particle in particles], axis=0)
+    return np.sum([particle.speed * particle.size ** 2 for particle in particles], axis=0)
 
 
-def elastic_collision(particle, other_particle):
+def elastic_collision(particle, particle2):
     """makes use of colliding() to compute speeds after eventual collision"""
-    if colliding(particle, other_particle):
-        corr = 2 * np.inner(np.array([particle.move_x - other_particle.move_x,
-                                      particle.move_y - other_particle.move_y]),
-                            np.array([particle.x - other_particle.x,
-                                      particle.y - other_particle.y]))
-
-        corr = np.divide(corr, np.linalg.norm(np.array([particle.x - other_particle.x,
-                                                        particle.y - other_particle.y])) ** 2)
-
-        corr = corr / (particle.size ** 2 + other_particle.size ** 2)
-
-        particle.move_x, particle.move_y = np.array([particle.move_x, particle.move_y]) \
-                                           - other_particle.size ** 2 \
-                                             * corr \
-                                             * np.array([particle.x - other_particle.x, particle.y - other_particle.y])
-
-        other_particle.move_x, other_particle.move_y = np.array([other_particle.move_x,
-                                                                 other_particle.move_y]) \
-                                                       - particle.size ** 2 \
-                                                         * corr \
-                                                         * np.array([other_particle.x - particle.x,
-                                                                     other_particle.y - particle.y])
+    corr = np.inner(particle.speed - particle2.speed, particle.pos - particle2.pos) * (particle.pos - particle2.pos)
+    corr = 2 * corr / ((particle.size ** 2 + particle2.size ** 2) * np.linalg.norm(particle.pos - particle2.pos) ** 2)
+    particle.speed = particle.speed - particle2.size ** 2 * corr
+    particle2.speed = particle2.speed + particle.size ** 2 * corr
 
 
 def draw_environment(particles):
     """the drawing of the frame"""
     momentum_gas = momentum(particles)
-    print("energy: {} \n  momentum of gas: {} \n momentum absorbed by wall: {}".format(energy(particles), momentum_gas,
-                                                                                       momentum_ini - momentum_gas))
+    # print("U: {} | P: {} | P_wall: {}".format(energy(particles), momentum_gas, momentum_ini - momentum_gas))
 
     game_display.fill(WHITE)
-
     for particle_pair in combinations(particles, 2):
-        elastic_collision(*particle_pair)
+        if colliding(*particle_pair):
+            elastic_collision(*particle_pair)
     for particle in particles:
-        pygame.draw.circle(game_display, particle.color,
-                           np.rint([particle.x, particle.y]).astype(int),
-                           particle.size)
+        pygame.draw.circle(game_display, particle.color, np.rint(particle.pos).astype('int64'), particle.size)
         particle.move()
 
     pygame.display.update()
@@ -90,7 +62,6 @@ def main():
     """the game, the largest scope before global"""
     global momentum_ini
     particles = [Particle(WIDTH, HEIGHT) for i in range(STARTING_BLUE_PARTICLES)]
-    particles.append(Particle(WIDTH, HEIGHT, RED))
     momentum_ini = momentum(particles)
     print("momentum ini: {}".format(momentum_ini))
     while True:
--- particle.py
+++ particle.py
@@ -1,28 +1,23 @@
 import random
 
-WHITE = (255, 255, 255)
+import numpy as np
+
 BLUE = (0, 0, 255)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
 
 
 class Particle:
-    def __init__(self, x_boundary, y_boundary, color=BLUE, size_range=(10, 11), max_ini_speed=10):
-        """it's a mess but it'll get more compact"""
+    def __init__(self, x_boundary, y_boundary, color=BLUE, size_range=(14, 15), max_ini_speed=10):
         L = [2 * i - max_ini_speed for i in range(max_ini_speed + 1) if i != max_ini_speed / 2]
         self.size = random.randrange(*size_range)
-        self.x = random.randrange(self.size, x_boundary - self.size)
-        self.y = random.randrange(self.size, y_boundary - self.size)
+        self.pos = np.random.rand(2) * ([x_boundary, y_boundary ] - np.array([self.size]*2)) + [self.size]*2
         self.color = color
         self.x_boundary = x_boundary
         self.y_boundary = y_boundary
-        self.move_x = random.choice(L)
-        self.move_y = random.choice(L)
+        self.speed = np.array([random.choice(L), random.choice(L)]).astype('float64')
 
     def move(self):
         """Essentially, bouncing from the walls"""
-        self.move_x = (2 * (self.size <= self.x <= self.x_boundary - self.size) - 1) * self.move_x
-        self.move_y = (2 * (self.size <= self.y <= self.y_boundary - self.size) - 1) * self.move_y
-
-        self.x += self.move_x
-        self.y += self.move_y
+        bounce_x = self.size <= self.pos[0] <= self.x_boundary - self.size
+        bounce_y = self.size <= self.pos[1] <= self.y_boundary - self.size
+        self.speed = np.array([2 * bounce_x - 1, 2 * bounce_y - 1]) * self.speed
+        self.pos += self.speed
