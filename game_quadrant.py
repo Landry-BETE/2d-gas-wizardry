0904bb8c42f8f988697b87b17b3b6c132a547ca3
Landry BETE <landrybete@gmail.com>
2018-03-22 18:52:37 +0000

    uploaded quadrant-based version. Next: quadtrees

---
+++ game_quadrant.py
@@ -0,0 +1,90 @@
+from itertools import *
+
+import pygame
+
+from particle import *
+
+
+
+
+STARTING_BLUE_PARTICLES = 200
+
+WIDTH = 700
+HEIGHT = 700
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+
+game_display = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Monoatomic Bidimensional Gas")
+clock = pygame.time.Clock()
+
+
+def colliding(pair):
+    particle, particle2 = pair
+    """checks if particles are overlaping & moving closer to one another to trigger bouncing & avoid agglomeration"""
+    if (np.linalg.norm(particle.pos - particle2.pos) - (particle.size + particle2.size) >= 0 ):
+        return False
+    return np.inner(particle.speed - particle2.speed, particle.pos - particle2.pos) <= 0
+
+
+def energy(particles):
+    """total kinetic energy up to a constant multiplier"""
+    return np.sum([particle.size ** 2 * np.linalg.norm(particle.speed) ** 2 for particle in particles])
+
+
+def momentum(particles):
+    """total momentum up to a constant multiplier"""
+    return np.sum([particle.speed * particle.size ** 2 for particle in particles], axis=0)
+
+
+def elastic_collision(particle, particle2):
+    """makes use of colliding() to compute speeds after eventual collision"""
+    corr = np.inner(particle.speed - particle2.speed, particle.pos - particle2.pos) * (particle.pos - particle2.pos)
+    corr = 2 * corr / ((particle.size ** 2 + particle2.size ** 2) * np.linalg.norm(particle.pos - particle2.pos) ** 2)
+    particle.speed = particle.speed - particle2.size ** 2 * corr
+    particle2.speed = particle2.speed + particle.size ** 2 * corr
+
+
+def draw_environment(particles):
+    """the drawing of the frame"""
+    # momentum_gas = momentum(particles)
+    # print("U: {} | P: {} | P_wall: {}".format(energy(particles), momentum_gas, momentum_ini - momentum_gas))
+    game_display.fill(WHITE)
+    particles1 = [p for p in particles if p.pos[0] < WIDTH / 2 and p.pos[1] < HEIGHT / 2]    
+    particles2 = [p for p in particles if p.pos[0] < WIDTH / 2 and p.pos[1] >= HEIGHT / 2]    
+    particles3 = [p for p in particles if p.pos[0] >= WIDTH / 2 and p.pos[1] < HEIGHT / 2]    
+    particles4 = [p for p in particles if p.pos[0] >= WIDTH / 2 and p.pos[1] >= HEIGHT / 2]    
+
+    pari = [particles1, particles2, particles3, particles4]
+
+    for particle in particles:
+        pygame.draw.circle(game_display, particle.color, np.rint(particle.pos).astype('int64'), particle.size)
+        particle.move()
+    pygame.display.update()
+
+    for particles in pari:
+        for particle_pair in combinations(particles, 2):
+         if colliding(particle_pair):
+            elastic_collision(*particle_pair)
+
+
+def main():
+    """the game, the largest scope before global"""
+    # global momentum_ini
+    particles = [Particle(WIDTH, HEIGHT) for i in range(STARTING_BLUE_PARTICLES)]
+    # momentum_ini = momentum(particles)
+    # print("momentum ini: {}".format(momentum_ini))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        draw_environment(particles)
+        clock.tick(100)
+
+
+if __name__ == '__main__':
+    main()
